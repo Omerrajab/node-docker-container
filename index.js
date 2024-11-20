96ae92796040a2fe59dfb479ffcb14e41800bbce
Enteromer@123# <omer.rajab@lelafe.com>
2024-11-20 14:48:37 +0530

    initial push

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+const PORT = process.env.PORT || 3000;
+console.log("port is:",process.env.PORT )
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
